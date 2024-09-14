8de67b74212d94eef45cacf9c3b98ef4a5975492
beukprincess <153230942+beukprincess@users.noreply.github.com>
2024-09-14 22:28:23 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'yourusername',
-  password: 'yourpassword',
-  database: 'yourdatabase'
+  host: '7.tcp.eu.ngrok.io',
+  user: 'root',
+  password: 'ridnashkola228208',
+  database: 'db_ged'
 });
 
-connection.connect();
+connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL database:', err.stack);
+      return;
+    }
+    console.log('Successfully connected to MySQL database as id ' + connection.threadId);
+  });
 
 connection.query('SELECT * FROM your_table', (error, results, fields) => {
   if (error) throw error;
@@ -20,3 +26,44 @@ fetch('http://localhost:5000/data')
   .catch(error => console.error('Error:', error));
 
 connection.end();
+
+function changePanel(item){
+    console.log("work");
+    let mainContent = document.getElementById("main");
+    let taskManagerContent = document.getElementById("task__manager");
+    let groupContent = document.getElementById("group");
+    let shopContent = document.getElementById("shop");
+    
+    if (item == "but__task__manager"){
+        mainContent.style.display = 'none';
+        taskManagerContent.style.display = 'block';
+        groupContent.style.display = 'none';
+        shopContent.style.display = 'none';
+
+        mainContent.style.height = '50vh';
+    }
+    else if (item == "but__main"){
+        mainContent.style.display = 'block';
+        taskManagerContent.style.display = 'none';
+        groupContent.style.display = 'none';
+        shopContent.style.display = 'none';
+
+        taskManagerContent.style.height = '100vh';
+    }
+    else if (item == "but__group"){
+        mainContent.style.display = 'none';
+        taskManagerContent.style.display = 'none';
+        groupContent.style.display = 'block';
+        shopContent.style.display = 'none';
+
+        groupContent.style.height = '100vh';
+    }
+    else {
+        mainContent.style.display = 'none';
+        taskManagerContent.style.display = 'none';
+        groupContent.style.display = 'none';
+        shopContent.style.display = 'block';
+
+        shopContent.style.height = '100vh';
+    }
+}
