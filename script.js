a482260f5569863283ab4e1bf5f8fa20c755bbad
beukprincess <153230942+beukprincess@users.noreply.github.com>
2024-09-15 00:08:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -27,53 +27,60 @@ fetch('http://localhost:5000/data')
 
 connection.end();
 
+
 function changePanel(item){
     let mainContent = document.getElementById("main");
     let taskManagerContent = document.getElementById("task__manager");
     let groupContent = document.getElementById("group");
     let shopContent = document.getElementById("shop");
+    let subject = document.getElementById('subject');
+
+    if(subject.style.backgroundColor == "#133C55"){
+        if (item == "but__task__manager"){
+            mainContent.style.display = 'none';
+            taskManagerContent.style.display = 'flex';
+            groupContent.style.display = 'none';
+            shopContent.style.display = 'none';
     
-    if (item == "but__task__manager"){
-        mainContent.style.display = 'none';
-        taskManagerContent.style.display = 'block';
-        groupContent.style.display = 'none';
-        shopContent.style.display = 'none';
-
-        mainContent.style.height = '50vh';
+            mainContent.style.height = '50vh';
+        }
+        else if (item == "but__main"){
+            mainContent.style.display = 'flex';
+            taskManagerContent.style.display = 'none';
+            groupContent.style.display = 'none';
+            shopContent.style.display = 'none';
+    
+            taskManagerContent.style.height = '100vh';
+        }
+        else if (item == "but__group"){
+            mainContent.style.display = 'none';
+            taskManagerContent.style.display = 'none';
+            groupContent.style.display = 'flex';
+            shopContent.style.display = 'none';
+    
+            groupContent.style.height = '100vh';
+        }
+        else {
+            mainContent.style.display = 'none';
+            taskManagerContent.style.display = 'none';
+            groupContent.style.display = 'none';
+            shopContent.style.display = 'flex';
+    
+            shopContent.style.height = '100vh';
+        }
     }
-    else if (item == "but__main"){
-        mainContent.style.display = 'block';
-        taskManagerContent.style.display = 'none';
-        groupContent.style.display = 'none';
-        shopContent.style.display = 'none';
-
-        taskManagerContent.style.height = '100vh';
-    }
-    else if (item == "but__group"){
-        mainContent.style.display = 'none';
-        taskManagerContent.style.display = 'none';
-        groupContent.style.display = 'block';
-        shopContent.style.display = 'none';
-
-        groupContent.style.height = '100vh';
-    }
-    else {
-        mainContent.style.display = 'none';
-        taskManagerContent.style.display = 'none';
-        groupContent.style.display = 'none';
-        shopContent.style.display = 'block';
-
-        shopContent.style.height = '100vh';
-    }
+    
 }
 function selectSubject(){
     console.log("work");
+    let subject = document.getElementById('subject');
+
     
-    if(subject.style.backgroundColor == 'rgb(255, 0, 0)'){
-        this.style.backgroundColor = 'green';
-    }
-    else{
-        this.style.backgroundColor = 'red';
-    }
+   // if(subject.style.backgroundColor == "red"){
+    //    subject.style.backgroundColor = 'green';
+    //}
+    //else{
+    //    subject.style.backgroundColor = 'red';
+    //}
 
 }
